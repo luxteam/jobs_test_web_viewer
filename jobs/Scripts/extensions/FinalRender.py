4f6c12023f620559d9b385008307437db8c98e48
Senya <senyakhodot1@gmail.com>
2022-10-02 15:40:25 +0300

    Additional test-cases and test-cases on samples Added waiting for render spinner to dissapear

--- jobs/Scripts/extensions/FinalRender.py
+++ jobs/Scripts/extensions/FinalRender.py
@@ -67,3 +67,97 @@ def test_010(args, case, driver, current_try):
     FinalRenderSteps.set_resolution(driver, "7680", "4320")
     FinalRenderSteps.start_render(driver)
     FinalRenderSteps.return_to_viewport(driver)
+
+def test_011(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_resolution(driver, "1280", "720")
+    FinalRenderSteps.start_render(driver)
+
+def test_012(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_format_to_png(driver)
+    FinalRenderSteps.set_resolution(driver, "1280", "720")
+    FinalRenderSteps.start_render(driver)
+
+def test_013(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_resolution(driver, "1135", "625")
+    FinalRenderSteps.start_render(driver)
+
+def test_014(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_format_to_png(driver)
+    FinalRenderSteps.set_resolution(driver, "1135", "625")
+    FinalRenderSteps.start_render(driver)
+
+def test_015(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_samples(driver, "9999")
+    FinalRenderSteps.start_render(driver)
+
+def test_016(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_format_to_png(driver)
+    FinalRenderSteps.set_samples(driver, "9999")
+    FinalRenderSteps.start_render(driver)
+
+def test_017(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_samples(driver, "1")
+    FinalRenderSteps.start_render(driver)
+
+def test_018(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_format_to_png(driver)
+    FinalRenderSteps.set_samples(driver, "1")
+    FinalRenderSteps.start_render(driver)
+
+def test_019(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_samples(driver, "1024")
+    FinalRenderSteps.start_render(driver)
+
+def test_020(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_format_to_png(driver)
+    FinalRenderSteps.set_samples(driver, "1024")
+    FinalRenderSteps.start_render(driver)
+
+def test_021(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_resolution(driver, "2048", "1080")
+    FinalRenderSteps.set_samples(driver, "9999")
+    FinalRenderSteps.start_render(driver)
+
+def test_022(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_format_to_png(driver)
+    FinalRenderSteps.set_resolution(driver, "2048", "1080")
+    FinalRenderSteps.set_samples(driver, "9999")
+    FinalRenderSteps.start_render(driver)
+
+def test_023(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_resolution(driver, "2048", "1080")
+    FinalRenderSteps.set_samples(driver, "1")
+    FinalRenderSteps.start_render(driver)
+
+def test_024(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_format_to_png(driver)
+    FinalRenderSteps.set_resolution(driver, "2048", "1080")
+    FinalRenderSteps.set_samples(driver, "1")
+    FinalRenderSteps.start_render(driver)
+
+def test_025(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_resolution(driver, "2048", "1080")
+    FinalRenderSteps.set_samples(driver, "1024")
+    FinalRenderSteps.start_render(driver)
+
+def test_026(args, case, driver, current_try):
+    FinalRenderSteps.open_final_render(driver)
+    FinalRenderSteps.set_format_to_png(driver)
+    FinalRenderSteps.set_resolution(driver, "2048", "1080")
+    FinalRenderSteps.set_samples(driver, "1024")
+    FinalRenderSteps.start_render(driver)
