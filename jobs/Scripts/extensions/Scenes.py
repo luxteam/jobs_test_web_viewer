8a1579e5b8da40e54ac6a57c16b6c26c7a73bae2
EvgeniyaKornishova <jennimyau@gmail.com>
2022-11-01 16:40:25 +0200

    clarified statuses in skipped in observed tests

--- jobs/Scripts/extensions/Scenes.py
+++ jobs/Scripts/extensions/Scenes.py
@@ -158,14 +158,12 @@ def test_022(args, case, driver, current_try):
     FinalRenderSteps.start_render(driver)
 
 def test_023(args, case, driver, current_try):
-    # TODO: set object name
-    ViewportSteps.select_element(driver, "<Object name>")
+    ViewportSteps.select_element(driver, "Sphere_001")
     
     LibrarySteps.click_library_tab(driver, 2)
     utils.choose_material("Gold", driver)
     sleep(3)
     LibrarySteps.click_library_tab(driver, 6)
-
 
 def test_024(args, case, driver, current_try):
     library = utils.find_by_xpath(ViewportLocators.LIBRARY, driver)
@@ -190,8 +188,7 @@ def test_024(args, case, driver, current_try):
 
 
 def test_025(args, case, driver, current_try):
-    # TODO: set object name
-    ViewportSteps.select_element(driver, "<Object name>")
+    ViewportSteps.select_element(driver, "Sphere_001")
     
     LibrarySteps.click_library_tab(driver, 2)
     utils.choose_material("Gold", driver)
@@ -201,8 +198,7 @@ def test_025(args, case, driver, current_try):
     LibrarySteps.click_library_tab(driver, 8)
 
 def test_026(args, case, driver, current_try):
-    # TODO: set object name
-    ViewportSteps.select_element(driver, "<Object name>")
+    ViewportSteps.select_element(driver, "Sphere_001")
 
     PropertiesSteps.open_properties(driver)
     PropertiesSteps.set(driver, "move", "X", "10")
@@ -253,7 +249,8 @@ def test_035(args, case, driver, current_try):
     FinalRenderSteps.start_render(driver)
 
 def test_036(args, case, driver, current_try):
-    ViewportSteps.select_element(driver, "Sphere_001")
+    #TODO: specify object
+    ViewportSteps.select_element(driver, "<object>")
     
     LibrarySteps.click_library_tab(driver, 2)
     utils.choose_material("Gold", driver)
@@ -283,98 +280,6 @@ def test_037(args, case, driver, current_try):
 
 
 def test_038(args, case, driver, current_try):
-    ViewportSteps.select_element(driver, "Sphere_001")
-    
-    LibrarySteps.click_library_tab(driver, 2)
-    utils.choose_material("Gold", driver)
-    sleep(3)
-    utils.choose_material("Aluminum", driver)
-    sleep(3)
-    LibrarySteps.click_library_tab(driver, 8)
-
-def test_039(args, case, driver, current_try):
-    ViewportSteps.select_element(driver, "Sphere_001")
-
-    PropertiesSteps.open_properties(driver)
-    PropertiesSteps.set(driver, "move", "X", "10")
-    PropertiesSteps.set(driver, "rotate", "Y", "-10")
-    PropertiesSteps.set(driver, "scale", "Z", "5")
-    sleep(8)
-
-def test_040(args, case, driver, current_try):
-    pass
-
-def test_041(args, case, driver, current_try):
-    ViewportSteps.rotate_scene(driver)
-
-def test_042(args, case, driver, current_try):
-    FinalRenderSteps.open_final_render(driver)
-    FinalRenderSteps.start_render(driver)
-
-def test_043(args, case, driver, current_try):
-    FinalRenderSteps.open_final_render(driver)
-    FinalRenderSteps.set_format_to_png(driver)
-    FinalRenderSteps.start_render(driver)
-
-def test_044(args, case, driver, current_try):
-    FinalRenderSteps.open_final_render(driver)
-    FinalRenderSteps.set_resolution(driver, "3840", "2160")
-    FinalRenderSteps.start_render(driver)
-
-def test_045(args, case, driver, current_try):
-    FinalRenderSteps.open_final_render(driver)
-    FinalRenderSteps.set_format_to_png(driver)
-    FinalRenderSteps.set_resolution(driver, "3840", "2160")
-    FinalRenderSteps.start_render(driver)
-
-def test_046(args, case, driver, current_try):
-    FinalRenderSteps.open_final_render(driver)
-    FinalRenderSteps.start_render(driver)
-    FinalRenderSteps.return_to_viewport(driver)
-
-def test_047(args, case, driver, current_try):
-    FinalRenderSteps.open_final_render(driver)
-    FinalRenderSteps.set_resolution(driver, "3840", "2160")
-    FinalRenderSteps.start_render(driver)
-    FinalRenderSteps.return_to_viewport(driver)
-
-def test_048(args, case, driver, current_try):
-    FinalRenderSteps.open_final_render(driver)
-    FinalRenderSteps.set_samples(driver, "100")
-    FinalRenderSteps.start_render(driver)
-
-def test_049(args, case, driver, current_try):
-    #TODO: specify object
-    ViewportSteps.select_element(driver, "<object>")
-    
-    LibrarySteps.click_library_tab(driver, 2)
-    utils.choose_material("Gold", driver)
-    sleep(3)
-    LibrarySteps.click_library_tab(driver, 6)
-
-def test_050(args, case, driver, current_try):
-    library = utils.find_by_xpath(ViewportLocators.LIBRARY, driver)
-    library.click()
-    sleep(1)
-    # TODO: evaluate object coordinates
-    #source_coords = utils.find_by_xpath("//div[ @class = 'click-area' ]//h2[ text() = 'Gold' ]", driver).location
-    sleep(1)
-    #pyautogui.moveTo(source_coords.get('x') + 30, source_coords.get('y') + 30)
-    pyautogui.moveTo(800, 750)
-    sleep(1)
-    pyautogui.mouseDown()
-    sleep(1)
-    pyautogui.moveTo(760, 480)
-    #pyautogui.moveRel(395, -290)
-    #pyautogui.moveRel(395, -445)
-    sleep(1)
-    pyautogui.mouseUp()
-    sleep(2)
-    library.click()
-    sleep(8)
-
-
-def test_051(args, case, driver, current_try):
     # TODO: specify object
     ViewportSteps.select_element(driver, "<object>")
     
@@ -385,7 +290,7 @@ def test_051(args, case, driver, current_try):
     sleep(3)
     LibrarySteps.click_library_tab(driver, 8)
 
-def test_052(args, case, driver, current_try):
+def test_039(args, case, driver, current_try):
     # TODO: specify object
     ViewportSteps.select_element(driver, "<object>")
 
