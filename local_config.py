2aea84b2cb0fb78399e4b8da5dfaf51ac7b36658
Speedwag00n <INemankov@luxoft.com>
2022-11-28 11:15:49 +0100

    Update local_config

--- local_config.py
+++ local_config.py
@@ -1,9 +1,14 @@
 tool_name = 'render_studio'
 report_type = 'default'
-groupped_tracked_metrics = {}
 show_skipped_groups = True
+tracked_metrics = {
+    'render_time': {'displaying_name': 'Render time', 'function': 'sum', 'displaying_unit': 's'}
+}
+tracked_metrics_files_number = 10000
+analyze_render_time = {"max_diff": 0.05}
 show_execution_time = True
-show_performance_tab = False
+show_performance_tab = True
+tracked_metrics_charts_location = 'performance'
 show_time_taken = False
 show_render_time = True
 hide_zero_render_time = True
