4dd72a5aca71886343e5f7716c1e6229de4f1fab
Senya <senyakhodot1@gmail.com>
2022-12-16 09:16:11 +0300

    Updated materials tests Focus on element Scene is moved after material apply

--- jobs/Scripts/locators.py
+++ jobs/Scripts/locators.py
@@ -2,6 +2,7 @@ class ViewportLocators(object):
     FINAL_RENDER = '//*[local-name() = "path" and @d="M4,4H7L9,2H15L17,4H20A2,2 0 0,1 22,6V18A2,2 0 0,1 20,20H4A2,2 0 0,1 2,18V6A2,2 0 0,1 4,4M12,7A5,5 0 0,0 7,12A5,5 0 0,0 12,17A5,5 0 0,0 17,12A5,5 0 0,0 12,7M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9Z"]'
     PROPERTIES = '//button/div[contains(text(), "Properties")]'
     FRIDGE = '//div[contains(text(), "Body")]'
+    FOCUS = '//*[local-name() = "path" and @d="M17 12C17 7.55 11.62 5.31 8.46 8.46C5.31 11.61 7.55 17 12 17C14.76 17 17 14.76 17 12M12 15C9.33 15 8 11.77 9.88 9.88C11.77 8 15 9.33 15 12C15 13.66 13.66 15 12 15M5 15H3V19C3 20.1 3.9 21 5 21H9V19H5M5 5H9V3H5C3.9 3 3 3.9 3 5V9H5M19 3H15V5H19V9H21V5C21 3.9 20.1 3 19 3M19 19H15V21H19C20.1 21 21 20.1 21 19V15H19"]/..'
     SCENE_INDEX_TAB = '//./button/div[ text() = "Scene Index" ]'
     SCENE_INDEX_TEXT = '//h3[ text() = "Scene Index" ]'
     SCENE_SEARCH = '//input[ @placeholder="Search Scene" ]'
